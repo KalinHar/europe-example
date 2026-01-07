6577b75f8b4f56e8108fcc4718a951ed26c91fe4
KalinHar <kalix@abv.bg>
2026-01-07 19:44:11 +0200

    Initial commit: Europe map

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <EuropeMap></EuropeMap>
+</template>
+
+<script>
+import EuropeMap from './components/EuropeMap.vue'
+
+export default {
+  name: 'App',
+  components: {
+    EuropeMap
+  }
+}
+</script>
+
+
+<style>
+/* .logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+} */
+</style>
